9a6c7553bb3f2211a8dd7407aff2b39fc55e8f6f
Ricardo Lamadrid <ricardo.lamadrid@bldr.com>
2022-12-17 16:58:17 -0600

    remove unecess

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { motion } from 'framer-motion';import { Tooltip as ReactTooltip } from 'react-tooltip'
 
 import { AppWrap, MotionWrap } from '../../wrapper';
-import { urlFor } from '../../client';
 import './Skills.scss';
 import { useSanityQuery } from '../../hooks/useSanityQuery';
 
 const Skills = () => {
-  const { skills } = useSanityQuery('skills');
   const { experiences } = useSanityQuery('experiences');
 
   return (
@@ -15,24 +13,6 @@ const Skills = () => {
       <h2 className="head-text">Experience</h2>
 
       <div className="app__skills-container">
-        {/* <motion.div className="app__skills-list">
-          {skills.map((skill) => (
-            <motion.div
-              whileInView={{ opacity: [0, 1] }}
-              transition={{ duration: 0.5 }}
-              className="app__skills-item app__flex"
-              key={skill.name}
-            >
-              <div
-                className="app__flex"
-                style={{ backgroundColor: skill.bgColor }}
-              >
-                <img src={urlFor(skill.icon)} alt={skill.name} />
-              </div>
-              <p className="p-text">{skill.name}</p>
-            </motion.div>
-          ))}
-        </motion.div> */}
         <div className="app__skills-exp">
           {experiences.map((experience) => (
             <motion.div
